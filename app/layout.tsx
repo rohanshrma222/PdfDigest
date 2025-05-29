2330c8ad36ec64042135e877381a9b4d6e78fad6
unknown <hackerviruse04@gmail.com>
2025-05-29 12:49:06 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PDFDIGEST- AI POWERED PDF SUMMARIZATION",
+  description: " AI-powered tool that instantly summarizes long PDFs into clear, concise insights. Upload your document and get the key points in seconds — save time, stay informed",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
